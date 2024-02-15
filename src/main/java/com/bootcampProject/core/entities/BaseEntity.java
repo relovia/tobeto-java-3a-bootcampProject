59d5ea15bc84cda42c46ba0f1852f8cbc0d0b01d
Yunus Emre <emrecivan06x@gmail.com>
2024-02-15 23:09:50 +0300

    Update BaseEntity

--- src/main/java/com/bootcampProject/core/entities/BaseEntity.java
+++ src/main/java/com/bootcampProject/core/entities/BaseEntity.java
@@ -1,11 +1,10 @@
 package com.bootcampProject.core.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,6 +12,16 @@ import lombok.NoArgsConstructor;
 @MappedSuperclass
 public class BaseEntity<T> {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private T id;
+
+    @Column(name="createdDate")
+    private LocalDateTime createdDate;
+
+    @Column(name="updatedDate")
+    private LocalDateTime updatedDate;
+
+    @Column(name="deletedDate")
+    private LocalDateTime deletedDate;
 }
